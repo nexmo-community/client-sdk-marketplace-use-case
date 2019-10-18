4584ade3a143a881be1b11a585c4761ff90c2bfc
Dwane <hey@dwane.io>
2019-10-18 16:27:36 -0400

    Adds dotenv

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -201,20 +202,6 @@ app.post('/stripePayment', function(req, res) {
         }
     })
 });
-
-
-// // http://expressjs.com/en/starter/static-files.html
-// app.use(express.static("public"));
-//
-// // http://expressjs.com/en/starter/basic-routing.html
-// app.get("/", function(request, response) {
-//     response.sendFile(__dirname + "/views/index.html");
-// });
-//
-// // listen for requests :)
-// const listener = app.listen(process.env.PORT, function() {
-//     console.log("Your app is listening on port " + listener.address().port);
-// });
 
 const listener = app.listen(3001, function() {
     console.log("Your app is listening on port " + listener.address().port);
